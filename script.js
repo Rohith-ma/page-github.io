e4b691a92a93b9773d2db1fe3e1485179a488a23
ROHITH_14 <127278669+Rohith-ma@users.noreply.github.com>
2024-02-13 19:15:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var pop=document.getElementById("pop")
+var popmsg=document.getElementById("popmsg")
+
+function update()
+{
+    pop.style.display="block";
+    popmsg.style.display="block";
+}
+function cancel()
+{
+    pop.style.display="none";
+    popmsg.style.display="none";
+}
